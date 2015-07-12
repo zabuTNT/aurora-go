d6af7ada5baacc94157712ed333d0ed2d331a769
Zabu <zabuza89@paranoici.org>
2015-07-12 14:29:20 +0200

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -6,7 +6,7 @@ Version:                0.1
 Date:                   12/07/2015
 Note:          		   Go 1.4.2
 **********************************************************************/
-package main
+package aurora_go
 
 import (
 	"encoding/json"
